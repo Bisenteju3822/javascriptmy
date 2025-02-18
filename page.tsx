e794d3d61ab7b5beee4998c1dc27202cd88df281
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-02-18 10:36:24 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/main/Hero";
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero/>
+      </div>
+    </main>
+  );
+}
