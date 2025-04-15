77b743e29d17e0d6768cb7023e0f382594c68aba
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-04-15 17:07:40 +0530

    Add files via upload

---
+++ practisevector.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	vector <int>  nums;
+	nums.push_back(10);
+	nums.push_back(20);
+	nums.push_back(30);
+
+	
+	for(int i=0;i<nums.size();i++){
+		cout<<i<<endl;
+		
+	}
+	for(int i:nums){
+		cout<<i<<endl;
+	}
+	
+	
+
+  
+
+	return 0;
+}
